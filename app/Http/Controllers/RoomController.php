2c8ebb9e54f6fd20fdf48b566f2bf158c676d55d
Kurosagi19 <thanhlanqwerty@gmail.com>
2025-06-23 10:19:11 +0700

    thêm các view mới, sửa các view cũ, thêm chức năng quản trị còn thiếu

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -13,7 +13,8 @@ class RoomController extends Controller
      */
     public function index()
     {
-        return view('Room.index');
+        $rooms = Room::all();
+        return view('Room.index', compact('rooms'));
     }
 
     /**
